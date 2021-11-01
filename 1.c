d99a5461dcfa1e52e2e44bdff79810a111e12425
My Name <myEnail@example.com>
2021-11-01 21:41:03 +0200

    get_commands work

--- 1.c
+++ 1.c
@@ -6,75 +6,113 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+void skip_spaces(char *ch) {
+	while(*ch == ' ')
+		*ch = getchar();
+}
 
-char *get_word(char *end, char *sign)
+char *get_word(char *end, char *sign, int *flag)
 {
     size_t len_w = 0;
     char *word = NULL, ch;
+	ch = getchar();
+	skip_spaces(&ch);
 	while(1) {
-		ch = getchar();
 		if(ch == ' ' || ch == '\t' || ch == '\n')
 			break;
 		if(ch == '<' || ch == '>') {
-			sign = ch;
+			*sign = ch;
 			ch = getchar();
-		skip_spaces(end);
+		//skip_spaces(end);
 		}
-		if(ch == '|')
+		if(ch == '|') {
+			*flag = 1;
+			break;
+		}
 		word = realloc(word, (len_w + 1) * sizeof(char));
 		word[len_w++] = ch;
-		}
+		ch = getchar();
+	}
+	*end = ch;
 	if (word == NULL)
 		return 0;
 	word = realloc(word, (len_w + 1) * sizeof(char));
 	word[len_w] = '\0';
-	*end = ch;
 	return word;
 };
 
-char **get_list(char sign, char *in_out[])
+char **get_list(char *sign, char *in_out[], char *end_of_w)
 {
 	size_t len_tx = 0;
-	char **text = NULL, end_of_w = 0;
+	char **text = NULL;
+	//size_t num_com = 0;
+	int flag = 0;
 	char *word = NULL;
 	while(1) {
-		if(end_of_w == '\n')
-			break;
-		word = get_word(&end_of_w, &sign);
-		if(sign && word) {
-			if(sign == '<')
+		word = get_word(end_of_w, sign, &flag);
+		/*if(*sign && word) {
+			if(*sign == '<')
 				in_out[0] = word;
-			else {
+			else if(*sign == '>') {
 				in_out[1] = word;
 			}
-		sign = 0;
-		continue;
+			*sign = 0;
+			continue;
+		}*/
+		if(flag) {
+			break;
 		}
 		text = realloc(text, (len_tx + 1) * sizeof(char*));
 		text[len_tx++] = word;
+		if(*end_of_w == '\n')
+			break;
 	}
-	if(text == NULL)
-		return 0;
 	text = realloc(text, (len_tx + 1) * sizeof(char*));
 	text[len_tx] = NULL;
 	return text;
 };
 
-
-char **free_list(char **text)
+char ***get_commands(char *in_out[], char ***mas_in_out)
 {
-	int ind = 0;
-	while(text[ind] != NULL) {
-		free(text[ind++]);
-	}
+		char ***all_com = NULL;
+		//char ***mas_in_out = NULL;
+		char **text = NULL;
+		int num_com = 0;
+		char *sign = NULL, end = 0;
+		text = get_list(sign, in_out, &end);
+		while(1) {
+        	all_com = realloc(all_com, (num_com + 1) * sizeof(char**));
+        	mas_in_out = realloc(mas_in_out, (num_com + 1) * sizeof(char**));
+        	mas_in_out[num_com] = in_out;
+        	all_com[num_com++] = text;
+        	sign = NULL;
+			if(end == '\n')
+				break;
+			text = get_list(sign, in_out, &end);
+
+    }
+    if(mas_in_out != NULL) {
+        mas_in_out = realloc(mas_in_out, (num_com + 1) * sizeof(char**));
+        mas_in_out[num_com] = NULL;
+    }
+    all_com = realloc(all_com, (num_com + 1) * sizeof(char**));
+    all_com[num_com] = NULL;
+    return all_com;
+};
+
+char ***free_list(char ***text)
+{
+	for (int i = 0; text[i]; i++)
+		for(int j = 0; text[i][j]; j++)
+			free(text[i][j]);
 	free(text);
 }
 
-int exit_proc(char **list)
+int exit_proc(char ***list)
 {
-	if(list == NULL || list[0] == NULL)
+	if(*list == NULL || *list[0] == NULL || list == NULL)
 		return 0;
-	if(strcmp(list[0], "exit") == 0 || strcmp(list[0], "quit") == 0)
+	if(strcmp(*list[0], "exit") == 0 || strcmp(*list[0], "quit") == 0)
 		return 1;
 	return 0;
 }
@@ -103,7 +141,7 @@ int redir_out(char *in_out) {
 	return x;
 }
 
-int exec_proc(char **list, char *in_out[])
+/*int exec_proc(char **list, char *in_out[])
 {
 	int fd[2];
 	fd[0] = redir_in(in_out[0]);
@@ -119,17 +157,6 @@ int exec_proc(char **list, char *in_out[])
 		}
 	}
 	return 0;
-}
-
-int is_delimeter(char ch) {
-	if(ch == '|')
-	
-
-}
-
-void exec_single(char **command, int input, int output) {
-	pid = fork();
-	
 }
 
 int exec_proc_per_few_command(char **list[], char *in_out[])
@@ -166,50 +193,34 @@ int exec_proc_per_few_command(char **list[], char *in_out[])
 	wait(NULL);
 	wait(NULL);
 	return 0;
-}
+}*/
 
-void func_few_comm(char **x, int num_of_com, char **command)
-	if(command) {
-		x = realloc(x, (num_of_com + 1) * sizeof(char**));
-		x[num_of_com++] = command;
-	}
-	x = realloc(x, (num_of_com + 1) * sizeof(char**));
-    x[num_of_com] = NULL;
-	return;
-}
-
-char ***get_commands(char sign, char *in_out[])
+void putline(char ***line)
 {
-    size_t num_com = 0;
-    char ***all_com = NULL;
-    char **command = NULL;
-    while(1) {
-        command = get_list(0, char *in_out[]);
-        all_com = realloc(all_com, (num_com + 1) * sizeof(char**));
-        all_com[num_com++] = command;
-    }
-    if(all_com == NULL)
-        return 0;
-    all_com = realloc(all_com, (num_com + 1) * sizeof(char**));
-    all_com[num_com] = NULL;
-    return all_com;
-};
+	if (line == NULL)
+		return;
+	for (int i = 0; line[i]; i++){
+		for(int j = 0; line[i][j]; j++)
+			printf("%s ", line[i][j]);
+		printf("#\n");
+	}
+	putchar('\n');
+}
 
 
 int main(int argc, char **argv)
 {
-	char ***all_commands = NULL;
 	char *in_out[] = {NULL, NULL};
-	char **mass_of_viv[] =
-	int num_of_com = 0;
-	char **command = get_list(0, in_out);
-	func_few_comm(all_commands[num_of_com], num_of_com, command);
-	while(!exit_proc(command)) {
-		exec_proc(command, in_out);
-		command = free_list(command);
-		command = get_list(0, in_out);
-		func_few_comm(all_commands[num_of_com], num_of_com, command);
+	char ***mas_in_out = NULL;
+	char ***all_commands = get_commands(in_out, mas_in_out);
+	while(!exit_proc(all_commands)) {
+		//exec_proc(command, in_out);
+//		putline(all_commands);
+		all_commands = free_list(all_commands);
+		all_commands = get_commands(in_out, mas_in_out);
+//		putline(all_commands);
+//		all_commands = get_commands(char *in_out[], char ***mas_in_out, 0, 0);
 	}
-	command = free_list(command);
+	//all_commands = free_list(all_commands);
 	return  0;
 }
