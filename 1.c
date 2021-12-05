039dbbe74d6d8118d226f60a540be0f4df889d85
My Name <myEnail@example.com>
2021-12-05 15:46:34 +0200

    with pipe

--- 1.c
+++ 1.c
@@ -15,14 +15,22 @@ void skip_spaces(char *ch) {
 		*ch = getchar();
 }
 
-char ***free_list(char ***text)
-{
+char ***free_list(char ***text) {
 	for (int i = 0; text[i]; i++){
 		for(int j = 0; text[i][j]; j++)
 			free(text[i][j]);
 		free(text[i]);
 	}
 	free(text);
+}
+
+char **free_in_out(char **text) {
+	int ind = 0;
+    while(text[ind] != NULL) {
+        free(text[ind++]);
+    }
+    free(text[ind]);
+    free(text);
 }
 
 int exit_proc(char ***list)
@@ -36,7 +44,7 @@ int exit_proc(char ***list)
 
 int redir_in(char *in_out) {
 	int x = 0;
-	if(in_out) {
+   	if(in_out) {
 		x = open(in_out, O_RDONLY, S_IRUSR|S_IWUSR);
 		if(x < 0) {
 			perror("open input error");
@@ -88,20 +96,8 @@ char *get_word(char *end, char *sign, int *flag, int *phone, int *conv)
 			ch = getchar();
 			skip_spaces(&ch);
 		}
-		if(ch == '|' && *flag) {
-			*conv == 2;
-			break;
-		}
 		if(ch == '|') {
 			*flag = 1;
-			break;
-		}
-		if(ch == '&' && *phone) {
-			*conv == 1;
-			break;
-		}
-		if(ch == '&') {
-			*phone == 1;
 			break;
 		}
 		word = realloc(word, (len_w + 1) * sizeof(char));
@@ -135,17 +131,31 @@ char **get_list(char *sign, char *in_out[], char *end_of_w, int *phone, int *con
 				break;
 			continue;
 		}
-		if(flag || *phone || *conv)
+		if(flag)
 			break;
 		text = realloc(text, (len_tx + 1) * sizeof(char*));
 		text[len_tx++] = word;
 		if(*end_of_w == '\n')
 			break;
 	}
+	if(text == NULL)
+		return 0;
 	text = realloc(text, (len_tx + 1) * sizeof(char*));
 	text[len_tx] = NULL;
 	return text;
 };
+
+void putline(char ***line)
+{
+	if (line == NULL)
+		return;
+	for (int i = 0; line[i]; i++){
+		for(int j = 0; line[i][j]; j++)
+			printf("%s ", line[i][j]);
+		printf("\n");
+	}
+	putchar('\n');
+}
 
 char ***get_commands(char *in_out[], int *num_com, int *phone, int *conv)
 {
@@ -216,95 +226,43 @@ int *exec_all(char *in_out[], char ***cmd, int n, int phone, int conv) {
 	return NULL;
 }
 
-/*
+
 void kill_pid(int *pid, int n) {
 	for(int i = 0; i < n; i++) {
 		if((kill(pid[i], 0) != 0))
 			kill(pid[i], SIGKILL);
 	}
 }
-*/
+
 void handler(int signo) {
 	puts("received SIGINT");
 	kill(son_pid, SIGKILL);
-}
-
-int conv_and(char ***list, char *in_out[], int n)
-{
-	int fd[2];
-	fd[0] = redir_in(in_out[0]);
-	fd[1] = redir_out(in_out[1]);
-	for(int i = 0; i < n; i++) {
-		if(fork() == 0) {
-			dup2(fd[0], 0);
-			dup2(fd[1], 1);
-			if(execvp(list[0][i], list[i]) < 0) {
-				perror("Exec failed");
-				exit(1);
-			}
-		}
-		int wstatus;
-		wait(&wstatus);
-		printf("%d\n", WEXITSTATUS(wstatus)); 
-		if(wstatus != 0)
-			break;
-		}
-	return 0;
-}
-
-int conv_or(char ***list, char *in_out[], int n)
-{
-	int fd[2];
-	fd[0] = redir_in(in_out[0]);
-	fd[1] = redir_out(in_out[1]);
-	for(int i = 0; i < n; i++) {
-		if(fork() == 0) {
-			dup2(fd[0], 0);
-			dup2(fd[1], 1);
-			if(execvp(list[0][i], list[i]) < 0) {
-				perror("Exec failed");
-				exit(1);
-			}
-		}
-		int wstatus;
-		wait(&wstatus);
-		printf("%d\n", WEXITSTATUS(wstatus)); 
-		if(wstatus == 0)
-			break;
-		}
-	return 0;
 }
 
 
 int main(int argc, char **argv)
 {
 	int n = 0;
-	char *in_out[] = {NULL, NULL};
 	char ***all_commands = NULL;
 	char *home = getenv("HOME");
 	int phone = 0, conv = 0;
-//	signal(SIGINT, handler);
+	signal(SIGINT, handler);
 	while(1) {
+		char *in_out[] = {NULL, NULL};
 		all_commands = get_commands(in_out, &n, &phone, &conv);
+		putline(all_commands);
 		if(exit_proc(all_commands))
 			break;
 		int *pid = NULL;
 		if(!change_directory(all_commands, home)) {
-			if(!conv) {
 			 pid = exec_all(in_out, all_commands, n, phone, conv);
-		//	if(pid != NULL)
-		//		kill_pid(pid, n);
-			}
-			else if(conv == 1) {
-				conv_and(all_commands, in_out, n);
-			}
-			else if(conv == 2) {
-				conv_or(all_commands, in_out, n);
-			}
+			if(pid != NULL)
+				kill_pid(pid, n);
 		}
 		all_commands = free_list(all_commands);
+    	free(in_out[0]);
+    	free(in_out[1]);
 		phone = 0, conv = 0;
-//		all_commands = get_commands(in_out, &n);
 	}
 	all_commands = free_list(all_commands);
 	return  0;
